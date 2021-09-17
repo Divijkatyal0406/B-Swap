bdb33186034adf95b9553c957621c12239f2202f
Divij Katyal <divijkatyal@gmail.com>
2021-09-17 19:34:01 +0530

    eth_swapp exchange

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,18 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          EthSwap
+          <large className="text-secondary">
+              EthSwapp Exchange
+            </large>
         </a>
 
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{this.props.account}</small>
+              <large id="account">{this.props.account}</large>
             </small>
 
             { this.props.account
